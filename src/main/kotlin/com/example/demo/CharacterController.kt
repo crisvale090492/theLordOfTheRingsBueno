1800799d3b4cfb2a6a08826e03305f186f46878f
valen <valenciamejia92@gmail.com>
2022-06-14 13:04:27 +0200

    mejorado

--- src/main/kotlin/com/example/demo/CharacterController.kt
+++ src/main/kotlin/com/example/demo/CharacterController.kt
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController
 class CharacterController {
 
     @GetMapping("getHumans")
-    fun gerHumans(): List<Doc> {
+    fun getHumans(): List<Doc> {
         var listaHumanos = mutableListOf<Doc>()
         characterList.docs.forEach {
             if (it.race == "Human")
@@ -17,10 +17,63 @@ class CharacterController {
         return listaHumanos
     }
 
-    //todo: getelfs
-    //todo: getorcs
-    //todo: getdragons
-    //todo: getgoodguys(elfs+humans)
-    //todo: getbadguys(orcs+dragons)
-    //todo: getothers(no good guy.no bad guy)
+    @GetMapping("getElfs")
+    fun getElfs(): List<Doc> {
+        val listaElfs = mutableListOf<Doc>()
+        characterList.docs.forEach {
+            if (it.race == "Elfs")
+                listaElfs.add(it)
+        }
+        return listaElfs
+    }
+
+    @GetMapping("getOrcs")
+    fun getOrcs(): List<Doc> {
+        val listaOrcs = mutableListOf<Doc>()
+        characterList.docs.forEach {
+            if (it.race == "Orcs")
+                listaOrcs.add(it)
+        }
+        return listaOrcs
+    }
+
+    @GetMapping("getDragons")
+    fun getDragons(): List<Doc> {
+        val listaDragons = mutableListOf<Doc>()
+        characterList.docs.forEach {
+            if (it.race == "Dragons")
+                listaDragons.add(it)
+        }
+        return listaDragons
+    }
+
+    @GetMapping("getGoodGuys")
+    fun getGoodGuys(): List<Doc> {
+        val listaGoodGuys = mutableListOf<Doc>()
+        characterList.docs.forEach {
+            if (it.race == "Humans" || it.race == "Elfs")
+                listaGoodGuys.add(it)
+        }
+        return listaGoodGuys
+    }
+
+    @GetMapping("getBadGuys")
+    fun getBadGuys(): List<Doc> {
+        val listaBadGuys = mutableListOf<Doc>()
+        characterList.docs.forEach {
+            if (it.race == "Orcs" || it.race == "Dragons")
+                listaBadGuys.add(it)
+        }
+        return listaBadGuys
+    }
+
+    @GetMapping("getOthers")
+    fun getOthers(): List<Doc> {
+        val listaOthers = mutableListOf<Doc>()
+        characterList.docs.forEach {
+            if (it.race != "Orcs" && it.race != "Dragons" && it.race != "Elfs" && it.race != "Humans")
+                listaOthers.add(it)
+        }
+        return listaOthers
+    }
 }
